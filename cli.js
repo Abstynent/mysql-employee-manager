57726bc3bf92424f907e4ed183081fdab95a5a90
Abstynent <wookash@Lukaszs-MacBook-Pro.local>
2023-04-13 20:03:54 +0100

    helper file to store all command-line prompt messages

---
+++ cli.js
@@ -0,0 +1,18 @@
+module.exports = cli = {
+    options: {
+        type: 'list',
+        name: 'userChoice',
+        message: 'Choose one:',
+        choices: ['View all departments', 'View all roles', 'View all employees',
+                  'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+    },
+    logo: `
+    --------------------------------------------------------------------
+     _____           _                    _____                         
+    |   __|_____ ___| |___ _ _ ___ ___   |     |___ ___ ___ ___ ___ ___ 
+    |   __|     | . | | . | | | -_| -_|  | | | | .'|   | .'| . | -_|  _|
+    |_____|_|_|_|  _|_|___|_  |___|___|  |_|_|_|__,|_|_|__,|_  |___|_|  
+                |_|       |___|                            |___|        
+               
+    --------------------------------------------------------------------`
+}
