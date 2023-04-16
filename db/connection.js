f004cc97d33eb9df8943dc96c56daaa7f05e1fcc
Abstynent <wookash@Lukaszs-MacBook-Pro.local>
2023-04-16 13:10:53 +0100

    database connection file created

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 module.exports = db = mysql.createConnection(
     {
-        host: 'localhost', 
+        host: '127.0.0.1', 
         user: 'root',
         password: '152413',
         database: 'employee_db'
